3852a24ab97f3073b2306a7c685d0ed50b0609bf
Amit <pardeshi.amitsingh@gmail.com>
2017-03-29 00:12:50 -0600

    [Amit] - removed unused functions

--- src/com/aos/pubsub/services/Main.java
+++ src/com/aos/pubsub/services/Main.java
@@ -65,28 +65,14 @@ public class Main extends Thread{
             //Printing the available services
             System.out.println("*********************************************************************************************");
             System.out.println("Type the action number as following:");
-            System.out.println("1. Search a file on the index server.");
-            System.out.println("2. Register a topic with eventbus.");
-            System.out.println("3. Register a message in topic");
-            System.out.println("4. Download file from a peer.");
-            System.out.println("5. List my files of the current directory.");
-            System.out.println("6. Calculate the performance of search requests.");
-            System.out.println("7. To exit.");
+            System.out.println("1. Register a topic with eventbus.");
+            System.out.println("2. Register a message in topic");
+            System.out.println("3. To exit.");
             System.out.println("*********************************************************************************************\n");
             Scanner in = new Scanner(System.in);
             userInput = in.nextLine();                         //get the chosen service from the user
-            /////////////////////////////////////////////////////////////////////////////
-            if (userInput.equals("1"))                         //if user input is 1
-            {
-                System.out.println("Enter the file name along with the file extension");
-                userInput = in.nextLine();                     //get file name that user want to search
-                if(fh.isValidName(userInput))                  //local validity of file name to enhance the performance
-                    fh.Search_for_a_File(userInput);           //call search function and attach the file name
-                else
-                    System.out.println("Wrong file format !"); //wrong file name format
-            }
-            /////////////////////////////////////////////////////////////////////////////
-            else if (userInput.equals("2"))                    //if user entered 2
+           
+             if (userInput.equals("1"))                    //if user entered 2
             {
                 System.out.println("Enter the topic name");
                 String topicName = in.nextLine();                     // get file name that user want to register
@@ -98,7 +84,7 @@ public class Main extends Thread{
                 fh.publishTopic(mm);             //call register function and attach the file name
             }
             /////////////////////////////////////////////////////////////////////////////
-            else if (userInput.equals("3"))                    //if user entered 3
+            else if (userInput.equals("2"))                    //if user entered 3
             {
             	 System.out.println("Enter the topic name");
                  String topicName = in.nextLine();                     // get file name that user want to register
@@ -109,38 +95,8 @@ public class Main extends Thread{
                //  tModel.setTopicName(topicName);
                  fh.publishTopic(message);             //call register function and attach the file name
             }
-            /////////////////////////////////////////////////////////////////////////////
-            else if (userInput.equals("4"))                    //if user entered 4
-            {
-
-                fh.Download_From_Peer();                       //call downloading function
-            }
-            /////////////////////////////////////////////////////////////////////////////
-            else if (userInput.equals("5"))                    //if user entered 5
-            {
-                list_my_files();                               //exit the program
-            }
-            /////////////////////////////////////////////////////////////////////////////
-            else if (userInput.equals("6"))                    //if user entered 6
-            {
-                System.out.println("Enter the file name along with the file extension");
-                userInput = in.nextLine();                     // get file name that user want to register
-                if(fh.isValidName(userInput)) {
-                    System.out.println("Enter the required number of requests");
-                    int loop ;                    // get file name that user want to register
-                    if(in.hasNextInt()){
-                        loop = in.nextInt();
-                        System.out.println(loop+" search requests average rate is " + get_performance_measurement_for_search_request(fh, userInput.trim(),loop) + " ms.");
-                    }else{
-                        System.out.println("Wrong integer format !");
-                    }
-
-                }
-                else
-                    System.out.println("Wrong file format !"); //wrong file name format
-            }
-            /////////////////////////////////////////////////////////////////////////////
-            else if (userInput.equals("7"))                    //if user entered 6
+           
+            else if (userInput.equals("3"))                    //if user entered 6
             {
                 System.out.println("Exiting...");
                 System.exit(0);                         //exit the program
