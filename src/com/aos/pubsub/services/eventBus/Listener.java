00747645ef246de216f13dbe1fcc782e1f801dee
Khalid T. Mursi <khalid.mursi@ttu.edu>
2017-03-29 18:05:43 -0500

    Update Listener.java

--- src/com/aos/pubsub/services/eventBus/Listener.java
+++ src/com/aos/pubsub/services/eventBus/Listener.java
@@ -1,8 +1,11 @@
 package com.aos.pubsub.services.eventBus;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import com.aos.pubsub.services.components.Main;
 import com.aos.pubsub.services.model.Message;
 import com.aos.pubsub.services.model.MessageMarker;
 import com.aos.pubsub.services.model.TopicModel;
@@ -121,7 +125,7 @@ public class Listener extends Thread {
 
         finally {
             System.out.println("Type the action number as following:");
-            System.out.println("1. To exit.");
+            System.out.println("1. To exit.\n");
             Thread.currentThread().stop();
         }
     }
@@ -129,63 +133,73 @@ public class Listener extends Thread {
     /*********************************************************************************************/
 
     synchronized void Subscribe_Topic_Request() {
-        try {
-            String topicName;                             // define an integer peer ID which is the  peer port
-            List<Message> messageList;                    //String array used for splitting the received message
+        try{
+            String topicName,reply=null;                             // define an integer peer ID which is the  peer port
+            List<Message> messageList;
+            int peerID;
+            //String array used for splitting the received message
             /////////////////////////////////////////////////////////////////////////////
             String subIP = conn.getInetAddress().getHostName();    //save the peer IP into peerIP
             ObjectInputStream in = new ObjectInputStream(conn.getInputStream()); //initiate object input stream to read from peer
-            MessageMarker messageMarker;
+           // MessageMarker messageMarker;
             String recievedString = null;
-            
-            try{
-            	recievedString = (String) in.readObject();               //read
-            	messageMarker = mapper.readValue(recievedString, SubscribtionModel.class);
-           }catch(JsonMappingException  | JsonParseException jEx){
-        	   messageMarker =  mapper.readValue(recievedString, Message.class);
-           }
-            SubscribtionModel topic = null;
-            Message messageModel = null;
-            
-            if(messageMarker instanceof Message){
-            	messageModel = (Message)messageMarker;
-            	String topicNameStr = messageModel.getTopicName();
-            	messageList  = indexBus.get(topicNameStr);
-            	Message m = new Message(messageList.size(), messageModel.getData(),topicNameStr );
-            	if(messageList != null){
-            		messageList.add(m);
+            recievedString=(String)in.readObject();
+            if(!recievedString.equals(null))
+            {
+            	String messageArray [] = recievedString.split("-");
+            	peerID = Integer.parseInt(messageArray[0]);
+            	topicName = messageArray[1];
+            	if(indexBus.containsKey(topicName))
+            	{
+            		System.out.println("Subscribtion request from "+subIP+":"+peerID+" accepted for topic "+topicName+"\n");
+                	reply="You are subcribed to "+topicName;
+                	Subscribtion_Recorder(subIP+"-"+recievedString);
             	}
-            	indexBus.put(topicNameStr, messageList);
-            	System.out.println("Added new message  "+messageModel.getData() + " in topic "+topicNameStr );
-            }else{
-            	System.out.println("Invalid object passed . returning....");
-            	
+            	else
+                {
+                	System.out.println("No message Received!\n");
+                	reply = "Topic not found\n";  
+                }
             }
-            
+            else
+            {
+            	System.out.println("No message Received!\n");
+            	reply = "Topic not found\n";  
+            }
+            ObjectOutputStream out = new ObjectOutputStream(conn.getOutputStream()); //define object writer
+            out.writeObject(reply);                                                  //write the reply to the peer
+            out.flush();
+            in.close();   
+            out.close();
+            conn.close();   
+        }
+        catch(Exception e)
+        {
+        	
+        }  
+    }
+    
+    public void Subscribtion_Recorder(String record) //write the downloaded file into the local director
+    {
+        try
+        {
+            //final File f = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()); //get the jar directory
+            //File parentFolder = new File(f.getParent());                                     //get the parent folder of the jar
+            //File folder = new File(parentFolder.getParent()+"/peer1/src/main/resources"); //get the resources folder path
+        	File folder = new File(".");
+            FileWriter writer = new FileWriter(folder+"/Subscribtion_Records.txt",true);//initiate writer
             /////////////////////////////////////////////////////////////////////////////
-                  //split the incoming message to adapt the local format
-
-             		// store peer ID
-            
-                in.close();                                         //close reader
-                conn.close();                                       //close connection
-            }
-        /////////////////////////////////////////////////////////////////////////////
-        catch(UnknownHostException unknownHost){                                           //To Handle Unknown Host Exception
+            writer.write(record+"\n");                                //write
+            writer.close();                                           //close writer
+        }
+        catch(UnknownHostException unknownHost){                      //To Handle Unknown Host Exception
             System.err.println("host not available..!");
         }
-        catch(IOException ioException){                                                    //To Handle Input-Output Exception
+        catch(IOException ioException){                               //To Handle Input-Output Exception
             ioException.printStackTrace();
         }
-         catch (Exception e) {                                      //track general errors
+        catch(Exception e){
             e.printStackTrace();
-            System.out.println(e.toString());
-        }
-
-        finally {
-            System.out.println("Type the action number as following:");
-            System.out.println("1. To exit.");
-            Thread.currentThread().stop();
         }
     }
     
@@ -245,7 +259,7 @@ public class Listener extends Thread {
 
         finally {
             System.out.println("Type the action number as following:");
-            System.out.println("1. To exit.");
+            System.out.println("1. To exit.\n");
             Thread.currentThread().stop();
         }
     }
